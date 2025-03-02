34f69e0cc42a9d8040e6bb76d537423a8ad52d38
Gideon22iboyi <your-email@example.com>
2025-03-02 20:22:57 +0100

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express= require('express');
 const Router= require('./router.js')
+const cors = require('cors');
 
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
